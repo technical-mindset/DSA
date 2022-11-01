d8736a7ad101826e65560cf9e3603249f7df1471
Asad <73779880+technical-mindset@users.noreply.github.com>
2022-11-01 15:17:00 +0500

    Updated LinkedList

--- Linked List/Main.java
+++ Linked List/Main.java
@@ -12,12 +12,21 @@ public class Main {
     }
 
 }
+
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        next = null;
+    }
+}
+
 class LinkedList{
     Node head;
 
     public void insert (int data){
     Node new_node = new Node(data);
-
         if (head == null) {
              head = new_node;
         }
@@ -29,6 +38,70 @@ class LinkedList{
             node.next = new_node;
         }
     }
+
+    public void insertAtStart (int data) {
+    Node new_node = new Node(data);
+    new_node.next = head;
+    head = new_node;
+    }
+    
+    public void insertAt (int index, int data) {
+        Node new_node = new Node(data);
+        Node node = head;
+        if (index > totalIteration()+1){
+            System.out.println("Irrelevant Position ");
+        }
+        else {
+            if (index == 1 || index == 0) {
+                insertAtStart(data);
+            } else {
+                for (int i = 0; i < index - 1; i++) {
+                    node = node.next;
+                }
+                new_node.next = node.next;
+                node.next = new_node;
+
+            }
+        }
+    }
+    //! This function calculate total index in LinkedList and return n-1 index
+    public int totalIteration () {
+        Node node = head;
+        int i = 0;
+        while(node.next != null) {
+            i++;
+            node = node.next;
+        }
+        return i;
+    }
+    
+    public void removeLast () {
+        Node node = head;
+        int j;
+        int i = totalIteration();
+        // iteration = 3 and node = 2nd last node
+        for (j = 0; j < i-1 ; j++) {
+            node = node.next;
+        }
+        node.next = null;
+
+
+    }
+    
+    public void removeAt (int index) {
+        if (index > totalIteration()+1) {
+            System.out.println("Irrelevant Position");
+        }
+        else {
+            Node node = head;
+            for (int i = 0; i+1 <index-1 ; i++) {
+                node = node.next;
+            }
+            node.next = node.next.next;
+        }
+
+    }
+    
     public void displayLinkedList(){
         Node node = head;
         while(node.next != null){
@@ -40,14 +113,7 @@ class LinkedList{
 
 
 }
-class Node{
-    int data;
-    Node next;
-    Node(int data){
-        this.data = data;
-        next = null;
-    }
-}
+
 
 
 
