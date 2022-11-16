7d4fe459a570b30a0003195b7d00ddd2cc8e86cd
Asad Zaidi <73779880+technical-mindset@users.noreply.github.com>
2022-11-16 16:26:21 +0500

    Bubble Sort uploaded

---
+++ Sorting/Bubble Sort/Sorting.java
@@ -0,0 +1,20 @@
+package DSA.Algorithms.Sorting;
+
+public class Sorting {
+    public static void BubbleSort(int[] arr){
+        // 8,2,6,4,1,9,10,15
+        int flag = 0;
+        int temp = -1;
+        while(flag < arr.length-1){
+            for (int i = 0; i < arr.length-1; i++) {
+                if (arr[i] > arr[i+1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+
+            }
+            flag++;
+        }
+    }
+}
