d1aeae580f7497a697b5133395c22b09bdd87c8b
Asad Zaidi <73779880+technical-mindset@users.noreply.github.com>
2022-12-03 14:52:45 +0500

    All patterns (uploaded 1 &2)

---
+++ Patterns/Patterns.java
@@ -0,0 +1,43 @@
+package DSA.Algorithms.Patterns;
+
+public class Patterns {
+
+    /* Pattern 1
+        *****
+        *****
+        *****
+    */
+
+    public static void pattern1(int n){
+        // for how many rows
+        for (int row = 0; row < n; row++) {
+            // for how many columns
+            for (int col = 0; col < n; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+    /* Pattern 2
+    
+     *
+     **
+     ***
+     ****
+
+     */
+
+    public static void pattern2(int n){
+        // for how many rows
+        for (int row = 0; row < n; row++) {
+            // for how many columns in each rows
+            for (int col = 0; col < row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
