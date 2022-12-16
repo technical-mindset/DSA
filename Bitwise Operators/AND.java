1bf05906fee1269e71cd8fd6ea530d3f715578d5
Asad Zaidi <73779880+technical-mindset@users.noreply.github.com>
2022-12-16 16:00:40 +0500

    set ith bit query uploaded

--- Bitwise Operators/AND.java
+++ Bitwise Operators/AND.java
@@ -8,15 +8,37 @@ public class Main {
     public static void main(String[] args) {
    
         Scanner sc = new Scanner(System.in);
+        oddEven(sc.nextInt());
+        
+        setBIT(1, 3);
+   
+   
+        }
+    
+     // Q1.) Find the given number is Odd or Even ?
+    
+      public static void oddEven(int n){
         System.out.print("Enter a number to check \"Even\" or \"Odd\": ");
         
         // odd numbers have '1' in the last digit and even have '0'
         // so when you take AND between a number and '1' if it is even the it returns true
         // other wise returns false
-        System.out.println((sc.nextInt() & 1) == 0);
+        System.out.println((n & 1) == 0);
+    }
    
-   
+    
+    
+    // Q2.) Set the i-th bit of n
+      public static void setBIT(int i, int n){
+        // taking '1' and apply left shift with i - 1;
+
+        if (i > 0) {
+            System.out.println(n & (1 << (i - 1)));
         }
+        else {
+            throw new RuntimeException("Left shift cant apply at: " + i + " position");
+        }
+    }
         
         
    }
