8e030cd6f07889d042f73982d8e77675bac10ebe
Asad Zaidi <73779880+technical-mindset@users.noreply.github.com>
2023-01-29 19:48:39 +0500

    Insertion function added

---
+++ Trees/AVL_Tree.java
@@ -0,0 +1,53 @@
+package DSA.Algorithms.Trees;
+
+public class AVLTree {
+ private AVL_Node root;
+  public AVLTree(){
+     this.root = null;
+ }
+ public void insertion(int data){
+   this.root = this.insertion(data, this.root);
+ }
+private AVL_Node insertion(int data, AVL_Node root){
+      // base-case: when the node is null then the creation of new Node and return
+      if(root == null) {
+          AVL_Node newNode = new AVL_Node(data);
+          return newNode;
+      }
+      else if (root.getData() > data) {
+          root.left = this.insertion(data, root.left);
+      }
+      else if (root.getData() < data) {
+          root.right = this.insertion(data, root.right);
+      }
+
+      // set height after each insertion
+      root.setHeight(Math.max(this.height(root.left),this.height(root.right)) + 1);
+      return root;
+
+
+}
+class AVL_Node{
+    AVL_Node left;
+    AVL_Node right;
+    int height;
+    private int data;
+    AVL_Node(int data){
+        this.data = data;
+        this.height = 1; // for easy calculation of balancing factor
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public int getData() {
+        return data;
+    }
+    public void setData(int data) {
+        this.data = data;
+    }
+}
+
